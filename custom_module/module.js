b527d5fdff559a72fb8e2a144b5666f3a3abd839
TorMari <mariia.torska.kn.2021@lpnu.ua>
2023-03-03 17:44:31 +0300

    commit

--- custom_module/module.js
+++ custom_module/module.js
@@ -27,23 +27,5 @@ exports.add_ProjProg = progress.add_ProjProg
 exports.get_progress_list = progress.get_progress_list;
 exports.getDoerProject = progress.getDoerProject;
 
-/*exports.find_Train = train.find_Train;
-exports.add_Train = train.add_Train;
-exports.edit_Train = train.edit_Train;
-exports.delete_Train = train.delete_Train;
-exports.maxSoldTickets = train.maxSoldTickets;
-exports.minSoldTickets = train.minSoldTickets;
-exports.get_trains_list = train.get_trains_list;
-
-exports.find_Ticket = ticket.find_Ticket;
-exports.add_Ticket = ticket.add_Ticket;
-exports.get_Ticket = ticket.get_Ticket;
-exports.get_ticket_list = ticket.get_ticket_list;
-
-exports.find_SoldTicket = sold_ticket.find_SoldTicket;
-exports.add_SoldTicket = sold_ticket.add_SoldTicket;
-exports.edit_Ticket = sold_ticket.edit_Ticket;
-exports.remove_SoldTicket = sold_ticket.remove_SoldTicket;
-exports.get_sold_ticket_list = sold_ticket.get_sold_ticket_list;*/
 
 
